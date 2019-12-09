e3cafe8dba205632aed1ffc26027b4ba2d4c56ae
Silvershot335 <silvershot335@gmail.com>
2019-12-08 18:57:14 -0600

    lol im dumb

---
+++ Backwards.py
@@ -0,0 +1,8 @@
+a = "judah is judy"
+
+b = a.rsplit(" ")
+
+b.reverse()
+
+print(" ".join(b))
+
---
+++ Calculator.py
@@ -0,0 +1,17 @@
+x = int(input())
+a = input()
+y = int(input())
+b = "+"
+c = "-"
+d = "*"
+e = "/"
+array = a
+
+
+if a.count(b): print (x + y);
+
+if a.count(c): print (x - y);
+
+if a.count(d): print (x * y);
+
+if a.count(e): print (x / y);
