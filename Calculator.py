a4b1d2aba25eb76aadc5b20007f51ab685cbf6ef
Silvershot335 <silvershot335@gmail.com>
2019-12-08 19:19:00 -0600

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -5,13 +5,11 @@ b = "+"
 c = "-"
 d = "*"
 e = "/"
-array = a
 
+if a.count(b): print (x + y)
 
-if a.count(b): print (x + y);
+if a.count(c): print (x - y)
 
-if a.count(c): print (x - y);
+if a.count(d): print (x * y)
 
-if a.count(d): print (x * y);
-
-if a.count(e): print (x / y);
+if a.count(e): print (x / y)
