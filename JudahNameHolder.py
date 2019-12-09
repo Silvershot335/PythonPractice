8d1e2c2d9debd44f91bb5e50d5dea8008c1d4811
Silvershot335 <silvershot335@gmail.com>
2019-12-08 19:04:32 -0600

    new toy

---
+++ JudahNameHolder.py
@@ -0,0 +1,13 @@
+with open('namesforjudah.txt', 'r') as a:
+    print(a.read())
+
+with open('namesforjudah.txt', 'a') as b:
+
+
+    if 'new' in input():
+        print("Enter New Nickname") 
+        b.write(", " + input(""))
+ 
+with open('namesforjudah.txt', 'r') as c:
+
+    print(c.read())
